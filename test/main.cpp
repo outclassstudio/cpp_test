b6cafcca8720a953cfa4e4476262231010090317
outclassstudio <mlimshs@gmail.com>
2025-02-26 00:23:09 +0900

    mul function

--- test/main.cpp
+++ test/main.cpp
@@ -9,12 +9,18 @@ int sub(int a, int b) {
 	return a - b;
 }
 
+int mul(int a, int b) {
+	return a * b;
+}
+
 int main() {
 	int a = 100;
 	int result = add(3, 5);
 	int result_sub = sub(10, 20);
+	int result_mul = mul(10, 2);
 	cout << result << endl;
 	cout << a << endl;
 	cout << result_sub << endl;
+	cout << result_mul << endl;
 	return 0;
 }
