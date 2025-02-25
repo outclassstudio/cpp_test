ec6edb1458419dc7614b9672daf12594a962c365
outclassstudio <mlimshs@gmail.com>
2025-02-26 00:19:35 +0900

    sub function

--- test/main.cpp
+++ test/main.cpp
@@ -5,10 +5,16 @@ int add(int a, int b) {
 	return a + b;
 }
 
+int sub(int a, int b) {
+	return a - b;
+}
+
 int main() {
 	int a = 100;
 	int result = add(3, 5);
+	int result_sub = sub(10, 20);
 	cout << result << endl;
 	cout << a << endl;
+	cout << result_sub << endl;
 	return 0;
 }
